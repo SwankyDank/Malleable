00fdbb0f3816ef24ab0d859120f29a1c9c383cb5
Davidah121 <42045018+Davidah121@users.noreply.github.com>
2020-11-19 21:19:32 -0500

    Example Scene with pause menu

---
+++ Malleable Project/Assets/GamePauseScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePauseScript : MonoBehaviour
+{
+    private bool pauseActive = false;
+    private GameObject rootObject = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rootObject = GameObject.Find("Game");
+    }
+
+    public void setPauseValue(bool value)
+    {
+        if (value == true)
+        {
+            rootObject.transform.Find("PauseMenu").gameObject.SetActive(true);
+            rootObject.transform.Find("GameScreen").gameObject.SetActive(false);
+        }
+        else
+        {
+            rootObject.transform.Find("PauseMenu").gameObject.SetActive(false);
+            rootObject.transform.Find("GameScreen").gameObject.SetActive(true);
+        }
+        pauseActive = value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            setPauseValue(!pauseActive);
+        }
+    }
+}
---
+++ Malleable Project/Assets/PlayerMoveScript.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveScript : MonoBehaviour
+{
+    private bool moving = false;
+    private bool air = false;
+    [SerializeField] private float speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            moving = true;
+            Vector3 moveVec = new Vector3(1, 0, 0) * -speed * Time.deltaTime;
+
+            this.GetComponent<SpriteRenderer>().flipX = true;
+
+            transform.Translate(moveVec);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            moving = true;
+            Vector3 moveVec = new Vector3(1, 0, 0) * speed * Time.deltaTime;
+
+            this.GetComponent<SpriteRenderer>().flipX = false;
+
+            transform.Translate(moveVec);
+        }
+        else
+        {
+            moving = false;
+        }
+
+        Animator animator = this.GetComponent<Animator>();
+        animator.SetBool("Moving", moving);
+        animator.SetBool("Air", air);
+    }
+}
