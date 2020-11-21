7f9afe1ce969802f93f3fd5b28654b8e7073bbec
Davidah121 <42045018+Davidah121@users.noreply.github.com>
2020-11-20 20:46:14 -0500

    Fixed pause menu showing background, added music, added collision

---
+++ Malleable Project/Assets/Scripts/MusicLooper.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicLooper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float startLoop = 0f;
+    public float endLoop = 0f;
+
+    public bool loops = false;
+    public bool hasIntro = false;
+
+    public int loopTimes = -1;
+    private int timesLooped = 0;
+
+    private AudioSource k;
+    void Start()
+    {
+        k = GetComponent<AudioSource>();
+        Debug.Log(k);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (loops)
+        {
+            if (!hasIntro)
+            {
+                if (k.time < startLoop)
+                {
+                    k.time = startLoop;
+                }
+            }
+
+            if (k.time >= endLoop)
+            {
+                if (timesLooped != loopTimes)
+                {
+                    k.time = startLoop;
+
+                    if (loopTimes < 0)
+                        timesLooped++;
+                }
+
+            }
+        }
+    }
+}
---
+++ Malleable Project/Assets/followMainCam.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followMainCam : MonoBehaviour
+{
+    private GameObject camObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camObject = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = camObject.transform.position;
+        this.transform.rotation = camObject.transform.rotation;
+
+    }
+}
